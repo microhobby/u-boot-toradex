nxp-imx/uboot-imx

commit 7af6854dacab50440d395173b5a5dfa309fbaef3

MA-15321-2 Add command to get serial number
Add commands to support extract serial number from device.
Commands:
    $ fastboot oem get-serial-number
    $ fastboot get_staged <path-to-serial-number>

Test: serial number upload on imx8mm.

Change-Id: I5c905ab797d4fd28d76c8403914f191eaf2ef687
Signed-off-by: Ji Luo <ji.luo@nxp.com>
(cherry picked from commit 250ef119c1dc02908046113893df5eeb9ef40605)
(cherry picked from commit e4790456a5b19c4a108dc5b9ec02e81ee8e2752b)

drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -113,13 +113,16 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
-static char *get_serial(void)
+static char serial[IMX_SERIAL_LEN];
+
+char *get_serial(void)
 {
-#ifdef CONFIG_SERIAL_TAG
+#if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
 	struct tag_serialnr serialnr;
-	static char serial[32];
+	memset(serial, 0, IMX_SERIAL_LEN);
+
 	get_board_serial(&serialnr);
-	sprintf(serial, "%08x%08x", serialnr.high,      serialnr.low);
+	sprintf(serial, "%08x%08x", serialnr.high, serialnr.low);
 	return serial;
 #else
 	return NULL;

drivers/fastboot/fb_fsl/fb_fsl_command.c
@@ -62,7 +62,6 @@ static u32 fastboot_bytes_received;
  */
 static u32 fastboot_bytes_expected;
 
-
 /* Write the bcb with fastboot bootloader commands */
 static void enable_fastboot_command(void)
 {
@@ -584,6 +583,18 @@ static void flashing(char *cmd, char *response)
 			strcpy(response, "FAILGenerate mppubk failed!");
 		} else {
 			printf("mppubk generated!\n");
+			strcpy(response, "OKAY");
+		}
+	}  else if (endswith(cmd, FASTBOOT_GET_SERIAL_NUMBER)) {
+		char *serial = get_serial();
+
+		if (!serial)
+			strcpy(response, "FAILSerial number not support!");
+		else {
+			/* Serial number will not exceed 16 bytes.*/
+			strncpy(fastboot_buf_addr, serial, 16);
+			fastboot_bytes_received = 16;
+			printf("Serial number generated!\n");
 			strcpy(response, "OKAY");
 		}
 	}

drivers/fastboot/fb_fsl/fb_fsl_common.h
@@ -44,7 +44,10 @@ extern AvbAtxOps fsl_avb_atx_ops;
 extern AvbOps fsl_avb_ops;
 #endif
 
+#define IMX_SERIAL_LEN 32
+
 int get_block_size(void);
 void process_erase_mmc(const char *cmdbuf, char *response);
+char *get_serial(void);
 
 #endif	// FB_FSL_COMMON_H

include/fb_fsl.h
@@ -100,6 +100,7 @@
 #define FASTBOOT_APPEND_RSA_ATTESTATION_CERT_ENC  "append-rsa-atte-cert-enc"
 #define FASTBOOT_APPEND_EC_ATTESTATION_CERT_ENC  "append-ec-atte-cert-enc"
 #define FASTBOOT_GET_MPPUBK  "get-mppubk"
+#define FASTBOOT_GET_SERIAL_NUMBER  "get-serial-number"
 #endif
 
 #ifdef CONFIG_ANDROID_THINGS_SUPPORT

microhobby/u-boot-toradex

commit 45f8fd916e4f96e8f1f0f13fcb4359424a4f54c1

fastboot: fix undefined serial[] error
This change fixes build error when CONFIG_SERIAL_TAG is
not set and CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG is set.

Upstream-Status: Inappropriate [other]

Fixes a build failure, which was introduced by one of the downstream commits.

Fixes: 7af6854dacab ("MA-15321-2 Add command to get serial number")
Signed-off-by: Andrejs Cainikovs <andrejs.cainikovs@toradex.com>

drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -118,7 +118,7 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
-#ifdef CONFIG_SERIAL_TAG
+#if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
 static char serial[IMX_SERIAL_LEN];
 #endif
 
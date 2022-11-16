45f8fd916e4f96e8f1f0f13fcb4359424a4f54c1
Andrejs Cainikovs <andrejs.cainikovs@toradex.com>
2022-11-16 16:03:29 +0100

    fastboot: fix undefined serial[] error
    This change fixes build error when CONFIG_SERIAL_TAG is
    not set and CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG is set.
    
    Upstream-Status: Inappropriate [other]
    
    Fixes a build failure, which was introduced by one of the downstream commits.
    
    Fixes: 7af6854dacab ("MA-15321-2 Add command to get serial number")
    Signed-off-by: Andrejs Cainikovs <andrejs.cainikovs@toradex.com>

--- drivers/fastboot/fb_fsl/fb_fsl_getvar.c
+++ drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -118,7 +118,7 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
-#ifdef CONFIG_SERIAL_TAG
+#if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
 static char serial[IMX_SERIAL_LEN];
 #endif
 
